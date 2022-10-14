adfb2653ba640b966462075896be75e2beeecc89
Njaaga-Gakure <njaagagakure@gmail.com>
2022-10-14 08:05:34 +0000

    Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Starts the execution of the program
+ * Return: Returns 0 when program execution is successful
+ */
+int main(void)
+{
+	char progQuote[100];
+
+	strcpy(progQuote, "\"Programming is like building a multilingual puzzle");
+
+	puts(progQuote);
+
+	return (0);
+}
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -7,3 +7,5 @@
 2-assembler generates the assembly code of a C code and save it in an output file
 
 3-name compiles a C file and creates an executable named cisfun
+
+4-puts.c prints exactly "Programming is like building a multilingual puzzle, followed by a new line
