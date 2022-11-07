346cfe9e012d8c319f09ba50908c2c9eb4165ab7
Njaaga-Gakure <njaagagakure@gmail.com>
2022-11-07 10:12:38 +0000

    Write a program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - prints its name, followed by a new line
+ * @argc: number of elements in the argv array
+ * @argv: pointer to a array of strings
+ * Return: returns 0 if successful
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
