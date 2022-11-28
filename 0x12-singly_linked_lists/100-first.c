390d676ff3ece4d75b7b6b723d577eb607e893e6
Njaaga-Gakure <njaagagakure@gmail.com>
2022-11-28 13:23:18 +0000

    Write a function that prints a string before a string in the main function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * print_before_main - prints a string before the string
+ *                     in the main function
+ */
+
+void print_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
